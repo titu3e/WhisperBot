41126d31f88e602230f2cea0f6f79a289c866a2c
titu3e <75441242+titu3e@users.noreply.github.com>
2021-11-14 20:21:32 +0530

    Update help.py

--- WhisperBot/help.py
+++ WhisperBot/help.py
@@ -8,6 +8,6 @@ from pyrogram.types import InlineKeyboardMarkup
 async def _help(bot, msg):
     await bot.send_message(
         msg.chat.id,
-        "**Here's how to use me 🔐**\n" + Data.HELP,
+        "**Here's how to Summon me 🔐**\n" + Data.HELP,
         reply_markup=InlineKeyboardMarkup(Data.home_buttons),
     )

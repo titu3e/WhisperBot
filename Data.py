3adbe2039e7680140cca3a6ec55fb2221a1ebac5
titu3e <75441242+titu3e@users.noreply.github.com>
2021-11-14 20:30:43 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -7,54 +7,52 @@ class Data:
 Hey {}.
 Welcome to {}
 
-I am the Master of Whisperers (like Varys in Game of Thrones).
+I am the Master of Shadows (like Shadows Of [Sungjinwoo](t.me/Sungjinwooarc). Enjoy Using me ).
 
-You can use me to send whispers to your friend in groups and channels (even if I'm not there).
-Only that friend and you will be able to read the message even though others are in same group. 
+You can use me to send Shadow Message to your Subordinate's in groups and channels (even if I'm not there).
+Only that Subordinate and you will be able to read the message even though others are in same group. 
 
-To see how to use me press 'How to Use' below.
+To see how to Summon me press 'How to Use' below.
 
-By @StarkBots
+By @Sungjinwooarc
     """
 
     # Home Button
     home_buttons = [
-        [InlineKeyboardButton("🔒 Send a Whisper 🔒", switch_inline_query="")],
+        [InlineKeyboardButton("🔒 Send a Shadow 🔒", switch_inline_query="")],
         [InlineKeyboardButton(text="🏠 Return Home 🏠", callback_data="home")],
     ]
     # Rest Buttons
     buttons = [
         [
-            InlineKeyboardButton("🔒 Send a Whisper 🔒", switch_inline_query="")
+            InlineKeyboardButton("🔒 Send a Shadow 🔒", switch_inline_query="")
         ],
         [
-            InlineKeyboardButton("How to Use ❔", callback_data="help"),
+            InlineKeyboardButton("How to Summon ❔", callback_data="help"),
             InlineKeyboardButton("🎪 About 🎪", callback_data="about")
         ],
-        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/StarkBots")],
-        [InlineKeyboardButton("🎨 Support Group 🎨", url="https://t.me/StarkBotsChat")],
+        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/Sungjinwooarc")],
+        [InlineKeyboardButton("🎨 Support Group 🎨", url="https://t.me/Sungjinwooarc")],
     ]
 
     # Help Message
     HELP = """
 Just type the message in below format in any chat.
 
-`@WhisperStarkBot your_message friend_username/id`
+`@shadow_Whisper_Robot your_message Subordinates_username/id`
     """
 
     # About Message
     ABOUT = """
 **About This Bot** 
 
-Bot created by @StarkBots
+Bot created by @Sungjinwooarc
 
-Source Code : [Click Here](https://github.com/StarkBotsIndustries/WhisperBot)
+Source Code : You Wont get it. Its not public 
 
-Inspired By : nnbbot
+Inspired By : My Friends bullying 
 
-Framework : [Pyrogram](docs.pyrogram.org)
+Language : Human 😂
 
-Language : [Python](www.python.org)
-
-Developer : @StarkProgrammer
+Developer : @Sungjinwoo
     """
